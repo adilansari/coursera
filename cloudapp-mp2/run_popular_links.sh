9e8032fd0a0e98f27feb496a2be8650d64603a81
Adil Ansari <adil@chartboost.com>
2015-09-21 16:15:08 -0700

    E output instead of D

--- cloudapp-mp2/run_popular_links.sh
+++ cloudapp-mp2/run_popular_links.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 hadoop fs -mkdir -p /mp2/A-input
-hadoop fs -mkdir -p /mp2/D-output
+hadoop fs -mkdir -p /mp2/E-output
 
 rm -rf ./build/* ./TopPopularLinks.jar
 export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar
 hadoop com.sun.tools.javac.Main TopPopularLinks.java -d build
 jar -cvf TopPopularLinks.jar -C build/ ./
-hadoop fs -rm -r /mp2/D-output
-hadoop jar TopPopularLinks.jar TopPopularLinks -D N=5 /mp2/links /mp2/D-output
+hadoop fs -rm -r /mp2/E-output
+hadoop jar TopPopularLinks.jar TopPopularLinks -D N=5 /mp2/links /mp2/E-output
 
-hadoop fs -cat /mp2/D-output/part*
-hadoop fs -cat /mp2/D-output/part-r-00000 | sort -n -k2 | head -n 10
+hadoop fs -cat /mp2/E-output/part*
+hadoop fs -cat /mp2/E-output/part-r-00000 | sort -n -k2 | head -n 10
