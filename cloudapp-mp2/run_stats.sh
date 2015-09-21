be4e188a89fecb111caddf81b327752e3c0b5456
Adil Ansari <ansari.adil20@gmail.com>
2015-09-20 19:09:26 -0700

    adding N to scripts

--- cloudapp-mp2/run_stats.sh
+++ cloudapp-mp2/run_stats.sh
@@ -7,7 +7,7 @@ export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar
 hadoop com.sun.tools.javac.Main TopTitleStatistics.java -d build
 jar -cvf TopTitleStatistics.jar -C build/ ./
 hadoop fs -rm -r /mp2/C-output
-hadoop jar TopTitleStatistics.jar TopTitleStatistics  -D stopwords=/mp2/misc/stopwords.txt -D delimiters=/mp2/misc/delimiters.txt /mp2/titles /mp2/C-output
+hadoop jar TopTitleStatistics.jar TopTitleStatistics  -D stopwords=/mp2/misc/stopwords.txt -D delimiters=/mp2/misc/delimiters.txt -D N=5 /mp2/titles /mp2/C-output
 
 hadoop fs -cat /mp2/C-output/part*
 hadoop fs -cat /mp2/C-output/part-r-00000 | sort -n -k2 -r | head -n 100
