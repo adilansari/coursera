48c8d97bade4b34e617242080447ebf196e27f8c
Adil Ansari <adil@chartboost.com>
2015-09-20 23:15:37 -0700

    fixing nullWritable

--- cloudapp-mp2/run_orphan.sh
+++ cloudapp-mp2/run_orphan.sh
@@ -7,7 +7,7 @@ export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar
 hadoop com.sun.tools.javac.Main OrphanPages.java -d build
 jar -cvf OrphanPages.jar -C build/ ./
 hadoop fs -rm -r /mp2/D-output
-hadoop jar OrphanPages.jar OrphanPages  -D /mp2/links /mp2/D-output
+hadoop jar OrphanPages.jar OrphanPages /mp2/links /mp2/D-output
 
 hadoop fs -cat /mp2/D-output/part*
 hadoop fs -cat /mp2/D-output/part-r-00000 | sort -n -k2 -r | head -n 100
