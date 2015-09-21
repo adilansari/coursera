497d8acc9af237590248af3a291bccd3d84d032f
Adil Ansari <adil@chartboost.com>
2015-09-20 17:52:33 -0700

    top titles run script

--- cloudapp-mp2/run_title_count.sh
+++ cloudapp-mp2/run_title_count.sh
@@ -7,7 +7,7 @@ export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar
 hadoop com.sun.tools.javac.Main TitleCount.java -d build
 jar -cvf TitleCount.jar -C build/ ./
 hadoop fs -rm -r /mp2/A-output
-hadoop jar TitleCount.jar TopTitles  -D stopwords=/mp2/misc/stopwords.txt -D delimiters=/mp2/misc/delimiters.txt /mp2/titles /mp2/A-output
+hadoop jar TitleCount.jar TitleCount  -D stopwords=/mp2/misc/stopwords.txt -D delimiters=/mp2/misc/delimiters.txt /mp2/titles /mp2/A-output
 
 hadoop fs -cat /mp2/A-output/part*
 hadoop fs -cat /mp2/A-ouput/part-r-00000 | sort -n -k2 -r | head -n 100
