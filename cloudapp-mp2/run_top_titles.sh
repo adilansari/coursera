4a0b2525416afc63c911c4aa0f2a8d669b7c51e9
Adil Ansari <adil@chartboost.com>
2015-09-20 19:09:26 -0700

    adding N to scripts

--- cloudapp-mp2/run_top_titles.sh
+++ cloudapp-mp2/run_top_titles.sh
@@ -7,7 +7,7 @@ export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar
 hadoop com.sun.tools.javac.Main TopTitles.java -d build
 jar -cvf TopTitles.jar -C build/ ./
 hadoop fs -rm -r /mp2/B-output
-hadoop jar TopTitles.jar TopTitles  -D stopwords=/mp2/misc/stopwords.txt -D delimiters=/mp2/misc/delimiters.txt /mp2/titles /mp2/B-output
+hadoop jar TopTitles.jar TopTitles  -D stopwords=/mp2/misc/stopwords.txt -D delimiters=/mp2/misc/delimiters.txt -D N=5 /mp2/titles /mp2/B-output
 
 hadoop fs -cat /mp2/B-output/part*
 hadoop fs -cat /mp2/B-output/part-r-00000 | sort -n -k2 -r | head -n 100
