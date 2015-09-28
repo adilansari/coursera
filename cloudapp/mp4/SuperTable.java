bd847b096e4cf112ab3ad5f0757be9daf4261d9f
Adil Ansari <adil@chartboost.com>
2015-09-27 22:32:00 -0700

    renaming vars

--- cloudapp/mp4/SuperTable.java
+++ cloudapp/mp4/SuperTable.java
@@ -40,36 +40,36 @@ public class SuperTable{
       System.out.println(" Table created ");
 
       // Instantiating HTable class
-      HTable hTable = new HTable(config, "powers");
+      HTable hTable = new HTable(con, "powers");
 
       // Instantiating Put class row1
-      Put p = new Put(Bytes.toBytes("row1"));
-      p.add(Bytes.toBytes("personal"), Bytes.toBytes("hero"),Bytes.toBytes("superman"));
-      p.add(Bytes.toBytes("personal"), Bytes.toBytes("power"),Bytes.toBytes("strength"));
-      p.add(Bytes.toBytes("professional"), Bytes.toBytes("name"),Bytes.toBytes("clark"));
-      p.add(Bytes.toBytes("professional"), Bytes.toBytes("xp"),Bytes.toBytes("100"));
-      hTable.put(p);
+      Put p1 = new Put(Bytes.toBytes("row1"));
+      p1.add(Bytes.toBytes("personal"), Bytes.toBytes("hero"),Bytes.toBytes("superman"));
+      p1.add(Bytes.toBytes("personal"), Bytes.toBytes("power"),Bytes.toBytes("strength"));
+      p1.add(Bytes.toBytes("professional"), Bytes.toBytes("name"),Bytes.toBytes("clark"));
+      p1.add(Bytes.toBytes("professional"), Bytes.toBytes("xp"),Bytes.toBytes("100"));
+      hTable.put(p1);
 
       // Instantiating Put class row2
-      Put p = new Put(Bytes.toBytes("row2"));
-      p.add(Bytes.toBytes("personal"), Bytes.toBytes("hero"),Bytes.toBytes("batman"));
-      p.add(Bytes.toBytes("personal"), Bytes.toBytes("power"),Bytes.toBytes("money"));
-      p.add(Bytes.toBytes("professional"), Bytes.toBytes("name"),Bytes.toBytes("bruce"));
-      p.add(Bytes.toBytes("professional"), Bytes.toBytes("xp"),Bytes.toBytes("50"));
-      hTable.put(p);
+      Put p2 = new Put(Bytes.toBytes("row2"));
+      p2.add(Bytes.toBytes("personal"), Bytes.toBytes("hero"),Bytes.toBytes("batman"));
+      p2.add(Bytes.toBytes("personal"), Bytes.toBytes("power"),Bytes.toBytes("money"));
+      p2.add(Bytes.toBytes("professional"), Bytes.toBytes("name"),Bytes.toBytes("bruce"));
+      p2.add(Bytes.toBytes("professional"), Bytes.toBytes("xp"),Bytes.toBytes("50"));
+      hTable.put(p2);
 
       // Instantiating Put class row3
-      Put p = new Put(Bytes.toBytes("row3"));
-      p.add(Bytes.toBytes("personal"), Bytes.toBytes("hero"),Bytes.toBytes("wolverine"));
-      p.add(Bytes.toBytes("personal"), Bytes.toBytes("power"),Bytes.toBytes("healing"));
-      p.add(Bytes.toBytes("professional"), Bytes.toBytes("name"),Bytes.toBytes("logan"));
-      p.add(Bytes.toBytes("professional"), Bytes.toBytes("xp"),Bytes.toBytes("75"));
-      hTable.put(p);
+      Put p3 = new Put(Bytes.toBytes("row3"));
+      p3.add(Bytes.toBytes("personal"), Bytes.toBytes("hero"),Bytes.toBytes("wolverine"));
+      p3.add(Bytes.toBytes("personal"), Bytes.toBytes("power"),Bytes.toBytes("healing"));
+      p3.add(Bytes.toBytes("professional"), Bytes.toBytes("name"),Bytes.toBytes("logan"));
+      p3.add(Bytes.toBytes("professional"), Bytes.toBytes("xp"),Bytes.toBytes("75"));
+      hTable.put(p3);
 
       // Close table
       hTable.close();
 
-      HTable table = new HTable(config, "powers");
+      HTable table = new HTable(con, "powers");
       // Instantiate the Scan class
       Scan scan = new Scan();
 
