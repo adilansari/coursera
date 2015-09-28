6d9ebd3ce3c23234d8d462d79dced8479458a957
Adil Ansari <ansari.adil20@gmail.com>
2015-09-27 17:48:14 -0700

    normalizer

--- cloudapp/mp3/src/NormalizerBolt.java
+++ cloudapp/mp3/src/NormalizerBolt.java
@@ -28,7 +28,12 @@ public class NormalizerBolt extends BaseBasicBolt {
      2. remove the common words
 
     ------------------------------------------------- */
+    String word = tuple.getString(0);
+    word = word.trim().toLowerCase();
 
+    if (!commonWords.contains(word)){
+      collector.emit(new Values(word));
+    }
 
   }
 
