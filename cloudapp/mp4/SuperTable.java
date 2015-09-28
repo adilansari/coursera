29da0601507959c9306e7958ca99631591392788
Adil Ansari <adil@chartboost.com>
2015-09-27 22:34:59 -0700

    fixing scanner

--- cloudapp/mp4/SuperTable.java
+++ cloudapp/mp4/SuperTable.java
@@ -74,16 +74,14 @@ public class SuperTable{
       Scan scan = new Scan();
 
       // Scanning the required columns
-      scan.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("name"));
-      scan.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("city"));
+      scan.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("hero"));
 
       // Getting the scan result
       ResultScanner scanner = table.getScanner(scan);
 
       // Reading values from scan result
       for (Result result = scanner.next(); result != null; result = scanner.next())
-
-      System.out.println("Found row : " + result);
+      System.out.println("keyvalues: " + result);
 
       // Close the scanner
       scanner.close();
