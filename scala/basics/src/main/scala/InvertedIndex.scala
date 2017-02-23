f5033f441df4e015137d8f3b50f4218cea2d892d
Adil Ansari <ansari.adil20@gmail.com>
2017-02-22 23:43:14 -0800

    inverted index code

--- scala/basics/src/main/scala/InvertedIndex.scala
+++ scala/basics/src/main/scala/InvertedIndex.scala
@@ -5,4 +5,17 @@ case class User(name: String, id: Int)
 
 class InvertedIndex(val userMap: mutable.Map[String, User]) {
 
+  def tokenizeName(name: String): Seq[String] = {
+    name.split(" ").map(_.toLowerCase)
+  }
+
+  def add(term: String, user: User): Unit = {
+    userMap += term -> user
+  }
+
+  def add(user: User): Unit = {
+    tokenizeName(user.name).foreach {
+      term => add(term, user)
+    }
+  }
 }
